cecc4dba640f3302ed0e1f388abb42dfa5241130
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2023-03-11 23:16:08 -0500

    scrabble solver part 2.

--- scrabble_solver.py
+++ scrabble_solver.py
@@ -6,17 +6,17 @@ scores = dict(a=1, b=3, c=3, d=2, e=1, f=4, g=2, h=4, i=1, j=8, k=5, l=1, m=3, n
               v=4, w=4, x=8, y=4, z=10, _=0)
 
 
-def score_word(word):
-    return sum(scores[c] for c in word)
-
-
-def word_possible(word, rack):
+def score_word(word, rack):
+    word_counter = Counter(word)
     rack_counter = Counter(rack)
-    for c in word:
-        if rack_counter[c] == 0:
-            return False
-        rack_counter[c] -= 1
-    return True
+    leftover_letters = word_counter - rack_counter
+    if rack_counter['_'] != 0:
+        if leftover_letters.total() != rack_counter['_']:
+            return -1
+    elif leftover_letters.total() != 0:
+        return -1
+    used_letters = word_counter - leftover_letters
+    return sum(scores[k]*v for k, v in used_letters.items())
 
 
 rack = sys.argv[1].lower()
@@ -24,8 +24,10 @@ scrabble_words = list(map(str.lower, open('sowpods.txt').read().splitlines()))
 possible_words = []
 
 for word in scrabble_words:
-    if word_possible(word, rack):
-        possible_words.append((score_word(word), word))
+    score = score_word(word, rack)
+    if score > 0:
+        possible_words.append((score, word))
+
 
 possible_words.sort(reverse=True)
 
